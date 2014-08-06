a240f838979079571c23bca1dc01f0315effffcb
nekova <nekova07@gmail.com>
2014-08-06 20:43:45 +0900

    Add `webhp` to GOOGLE_URL

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,10 +1,12 @@
 'use strict';
 
-var GOOGLE_URL = new RegExp("^http(s)?:\/\/www\.google\.[a-z.]+\/(search|#)(\\?)?q=.*");
+var GOOGLE_URL = new RegExp("^http(s)?:\/\/www\.google\.[a-z.]+\/(webhp)?(search|#)(\\?)?q=.*");
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-  if(GOOGLE_URL.test(tab.url)) {
-    var newUrl = tab.url + "&tbs=qdr:y";
-    chrome.tabs.update(tab.id, {url: newUrl});
+chrome.browserAction.onClicked.addListener(
+  function(tab) {
+    if(GOOGLE_URL.test(tab.url)) {
+      var newUrl = tab.url + "&tbs=qdr:y";
+      chrome.tabs.update(tab.id, {url: newUrl});
+    }
   }
-});
+);
