293168eeddb16c855d9e5b1bfa792c0ea72ead3e
nekova <nekova07@gmail.com>
2014-08-12 13:28:52 +0900

    Add imageSearch

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -7,7 +7,11 @@ chrome.commands.onCommand.addListener(function (command) {
         if(GOOGLE_URL.test(tab.url)) {
             switch (command) {
                 case 'pastYear':
-                    var newUrl = tab.url + "&tbs=qdr:y";
+                    var newUrl = tab.url + '&tbs=qdr:y';
+                    chrome.tabs.update(tab.id, {url: newUrl});
+                    break;
+                case 'imageSearch':
+                    var newUrl = tab.url + '&tbm=isch'
                     chrome.tabs.update(tab.id, {url: newUrl});
                     break;
             }
