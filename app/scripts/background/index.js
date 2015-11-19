842ebb825a99afffeb9e4abf5cf9d5b37a0a1a9e
nekova <nekova07@gmail.com>
2015-11-19 16:17:23 +0900

    Change build tool to gulp

---
+++ app/scripts/background/index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+// let GOOGLE_URL = new RegExp('^http(s)?:\/\/www.google.[a-z.]+\/(?:webhp)?(?:search|(.*)#)(?:\\?)?q=.*');
+chrome.commands.onCommand.addListener(command => {
+    chrome.tabs.getSelected(null, tab => {
+        // if (GOOGLE_URL.test(tab.url)) {
+        switch (command) {
+        case 'pastYear':
+            var newUrl = `${ tab.url }&tbs=qdr:y`;
+            chrome.tabs.update(tab.id, { url: newUrl });
+            break;
+        case 'twoYearsAgo':
+            let today = new Date();
+            let twoYearsAgo = today.getFullYear() - 2;
+            let thisMonth = today.getMonth() + 1;
+            var newUrl = `${ tab.url }&tbs=cdr%3A1%2Ccd_min%3A${ twoYearsAgo }%2F${ thisMonth }%2F${ today.getDate() }`;
+            chrome.tabs.update(tab.id, { url: newUrl });
+            break;
+        }
+        // }
+    });
+});
+//
+// var GOOGLE_URL = new RegExp("^http(s)?:\/\/www\.google\.[a-z.]+\/(?:webhp)?(?:search|(.*)#)(?:\\?)?q=.*");
+//
+// chrome.commands.onCommand.addListener(function (command) {
+//     chrome.tabs.getSelected(null, function(tab){
+//         if(GOOGLE_URL.test(tab.url)) {
+//             switch (command) {
+//                 case 'pastYear':
+//                     var newUrl = tab.url + '&tbs=qdr:y';
+//                     chrome.tabs.update(tab.id, {url: newUrl});
+//                     break;
+//                 case 'twoYearsAgo':
+//                     var today = new Date();
+//                     var twoYearsAgo = today.getFullYear() - 2;
+//                     var thisMonth = today.getMonth()+1;
+//                     var newUrl = tab.url + "&tbs=cdr%3A1%2Ccd_min%3A" + twoYearsAgo +"%2F" + thisMonth + "%2F" + today.getDate();
+//                     chrome.tabs.update(tab.id, {url: newUrl});
+//                     break;
+//             }
+//         }
+//     })
+// });
